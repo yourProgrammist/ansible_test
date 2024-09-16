606e731325a115f9ed4615a7774bfced6bc6edb8
yourProgrammist <kiryusha.osin@yandex.ru>
2024-09-16 23:38:05 +0300

    add ansible02

---
+++ ansible02/roles/install_consul_client/files/envoy_install.sh
@@ -0,0 +1,2 @@
+curl -L https://github.com/envoyproxy/envoy/releases/download/v1.25.3/envoy-1.25.3-linux-aarch_64 -o /usr/local/bin/envoy
+chmod +x /usr/local/bin/envoy
---
+++ ansible02/roles/install_consul_server/files/consul_install.sh
@@ -0,0 +1,4 @@
+wget https://releases.hashicorp.com/consul/1.16.6/consul_1.16.6_linux_arm64.zip
+unzip consul_1.16.6_linux_arm64.zip
+mv consul /usr/local/bin/consul
+rm -rf consul_1.16.6_linux_arm64.zip
